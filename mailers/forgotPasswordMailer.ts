4b8677738c39e8b378bf05ca88dca79eab95482f
Shaun Hutch <shaun@promoboxx.com>
2022-01-28 15:12:23 -0500

    email config

--- mailers/forgotPasswordMailer.ts
+++ mailers/forgotPasswordMailer.ts
@@ -33,7 +33,7 @@ export function forgotPasswordMailer({ to, token }: ResetPasswordMailer) {
     async send() {
       if (process.env.NODE_ENV === "production") {
         try {
-          await await postmarkClient.sendEmail(message)
+          await await postmarkClient.send(message)
         } catch (err) {
           console.error(err)
         }
