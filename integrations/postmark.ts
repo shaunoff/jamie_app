873da1f6553cc18dc4e64cfcaf601d41b88ff3c9
Shaun Hutch <shaun@promoboxx.com>
2022-01-28 15:20:58 -0500

    email config

--- integrations/postmark.ts
+++ integrations/postmark.ts
@@ -1,5 +1,8 @@
-import postmark from "postmark"
+const postmark = require("postmark")
+import * as PostmarkTypes from "postmark"
 
-const client = new postmark.ServerClient(<string>process.env.POSTMARK_API_KEY)
+const client: PostmarkTypes.ServerClient = new postmark.ServerClient(
+  <string>process.env.POSTMARK_API_KEY
+)
 
 export default client
