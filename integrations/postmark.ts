1c8b58b2759b623d9e983afc25206f23caf8cfba
Shaun Hutch <shaun@promoboxx.com>
2022-01-28 15:24:09 -0500

    email config

--- integrations/postmark.ts
+++ integrations/postmark.ts
@@ -1,8 +1,9 @@
-const postmark = require("postmark")
-import * as PostmarkTypes from "postmark"
+var postmark = require("postmark")
+// import * as PostmarkTypes from "postmark"
 
-const client: PostmarkTypes.ServerClient = new postmark.ServerClient(
-  <string>process.env.POSTMARK_API_KEY
-)
+// const client: PostmarkTypes.ServerClient = new postmark.ServerClient(
+//   <string>process.env.POSTMARK_API_KEY || "xxx"
+// )
 
+var client = new postmark.ServerClient("9c6aeaad-149b-48b2-b78f-f811d6231e97")
 export default client
