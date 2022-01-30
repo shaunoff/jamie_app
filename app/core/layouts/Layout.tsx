a377d1b34430ea1a9993167feb971b730d7c5dfc
Shaun Hutch <shaun@promoboxx.com>
2022-01-30 11:49:11 -0500

    layout

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react"
 import { BlitzLayout, Image } from "blitz"
+import DefaultHeader from "./DeafultHeader"
 import sprightly from "public/logo_long.png"
 import { Dialog, Transition } from "@headlessui/react"
 import {
@@ -26,11 +27,12 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
 }
 
-export default function Example() {
+const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
     <>
+      <DefaultHeader title={title || ""} />
       <div>
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog as="div" className="fixed inset-0 flex z-40 md:hidden" onClose={setSidebarOpen}>
@@ -207,12 +209,14 @@ export default function Example() {
           <main className="flex-1">
             <div className="py-6">
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-                <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
                 <div className="py-4">
-                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+                    {children}
+                  </div>
                 </div>
                 {/* /End replace */}
               </div>
@@ -224,15 +228,4 @@ export default function Example() {
   )
 }
 
-// import { BlitzLayout } from "blitz"
-// import DefaultHeader from "./DeafultHeader"
-// const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
-//   return (
-//     <>
-//       <DefaultHeader title={title || ""} />
-//       {children}
-//     </>
-//   )
-// }
-
-// export default Layout
+export default Layout
