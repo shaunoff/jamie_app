97f45181560eff947c0c517c3d1df531a154c903
Shaun Hutch <shaun@promoboxx.com>
2022-02-03 21:12:40 -0500

    logo

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -78,9 +78,9 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
                 </Transition.Child>
                 <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
                   <div className="flex-shrink-0 flex items-center px-4">
-                    <div className="w-1/2">
+                    {/* <div className="w-1/2">
                       <Image src={sprightly} alt="sprightly" />
-                    </div>
+                    </div> */}
                   </div>
                   <nav className="mt-5 px-2 space-y-1">
                     {navigation.map((item) => (
@@ -142,9 +142,9 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex items-center flex-shrink-0 px-4 w-2/3">
+              {/* <div className="flex items-center flex-shrink-0 px-4 w-2/3">
                 <Image src={sprightly} alt="sprightly" />
-              </div>
+              </div> */}
               <nav className="mt-5 flex-1 px-2 bg-white space-y-1">
                 {navigation.map((item) => (
                   <a
