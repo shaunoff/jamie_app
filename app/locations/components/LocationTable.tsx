9fd08dcc0b00fdbdd2d96d1a56a875dba84a425d
Shaun Hutch <shaun@promoboxx.com>
2022-02-16 20:12:26 -0500

    suspense

--- app/locations/components/LocationTable.tsx
+++ app/locations/components/LocationTable.tsx
@@ -30,7 +30,7 @@ const LocationTable: React.FC<LocationTableProps> = ({ locations, admin = true }
             <a>Create Location</a>
           </Link> */}
           <button
-            onClick={() => router.push(Routes.NewLocationPage())}
+            onClick={() => router.push(Routes.NewLocation())}
             type="button"
             className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
