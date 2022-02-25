4dfe36e1bcb9729b1ebd2e1f1918eaae82316881
Shaun Hutch <shaun@promoboxx.com>
2022-02-24 20:53:52 -0500

    create user

--- app/users/mutations/createUser.ts
+++ app/users/mutations/createUser.ts
@@ -1,15 +1,22 @@
-import { resolver } from "blitz"
+import { resolver, SecurePassword } from "blitz"
 import db from "db"
 import { z } from "zod"
 
 const CreateUser = z.object({
+  name: z.string(),
   email: z.string(),
   password: z.string(),
+  role: z.string(),
 })
 
-export default resolver.pipe(resolver.zod(CreateUser), resolver.authorize(), async (input) => {
-  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const user = await db.user.create({ data: input })
+export default resolver.pipe(
+  resolver.zod(CreateUser),
+  resolver.authorize(),
+  async ({ password, name, email }) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const hashedPassword = await SecurePassword.hash(password.trim())
+    const user = await db.user.create({ data: { name, email, hashedPassword, role: "USER" } })
 
-  return user
-})
+    return user
+  }
+)
